8d968d56c86113942e571f5e315bc1a14fcabd15
Andrey Tokarev <abtokarev@gmail.com>
2018-07-07 19:27:34 +0300

    Using collection.Counter() instead of dict

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import Counter
 
 
 def load_data(filepath):
@@ -8,14 +9,9 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
-    freq_dict = dict()
-    for key in text:
-        if key in freq_dict:
-            freq_dict[key] += 1
-        else:
-            freq_dict[key] = 1
-    freq_dict = sorted(freq_dict.items(), key=lambda kv: -kv[1])
-    return freq_dict[:10]
+    symbols_counter = Counter(text)
+
+    return symbols_counter.most_common(10)
 
 
 if __name__ == '__main__':
