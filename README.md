532c0c834a9b5aa818013baea250e00a18dc9c98
Andrey Tokarev <abtokarev@gmail.com>
2018-07-07 18:03:05 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+This script loads text from file and prints top 10 frequent symbols with conunts from this text.
+
+# Quickstart
+
+Tu run on Linux, Python 3.5:
+
+```bash
+
+$ python lang_frequency.py <path to file>
+' ': 584873
+'о': 262804
+'е': 188636
+'а': 186189
+'и': 154497
+'н': 144144
+'т': 133153
+'с': 122433
+'л': 114800
+'в': 102592
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,29 @@
+import argparse
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as f:
+        text = f.read()
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    freq_dict = dict()
+    for key in text:
+        if key in freq_dict:
+            freq_dict[key] += 1
+        else:
+            freq_dict[key] = 1
+    freq_dict = sorted(freq_dict.items(), key=lambda kv: -kv[1])
+    return freq_dict[:10]
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+        description='Prints top 10 symbols with frequencies from text file.')
+    parser.add_argument('filepath', metavar='f', type=str,
+                        help='path to text file')
+    args = parser.parse_args()
+    text_loaded = load_data(args.filepath)
+    freq_dict = get_most_frequent_words(text_loaded)
+    print("\n".join(["'{0}': {1}".format(x[0], x[1]) for x in freq_dict]))
