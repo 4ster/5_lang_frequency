cb4a0a101f0ab0eed715f667d48b2698cf4b11a0
Andrey Tokarev <abtokarev@gmail.com>
2018-07-08 15:31:05 +0300

    Move parser creation in separate function

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,26 +1,35 @@
 import argparse
 from collections import Counter
 
-TOP_COUNT = 10
 
 def load_data(filepath):
-    with open(filepath) as f:
-        text = f.read()
+    with open(filepath) as textfile:
+        text = textfile.read()
     return text
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, top_count):
     symbols_counter = Counter(text)
+    return symbols_counter.most_common(top_count)
 
-    return symbols_counter.most_common(TOP_COUNT)
+
+def create_parser(top_count):
+    parser = argparse.ArgumentParser(
+        description="Prints top {0} symbols with it's frequencies from text file.".format(top_count))
+    parser.add_argument(
+        "filepath",
+        metavar="f",
+        type=str,
+        help="path to text file"
+    )
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Prints top {0} symbols with frequencies from text file.'.format(TOP_COUNT))
-    parser.add_argument('filepath', metavar='f', type=str,
-                        help='path to text file')
-    args = parser.parse_args()
+    top_count = 10
+    args = create_parser(top_count)
     text_loaded = load_data(args.filepath)
-    freq_dict = get_most_frequent_words(text_loaded)
-    print("\n".join(["'{0}': {1}".format(x[0], x[1]) for x in freq_dict]))
+    most_frequent_symbols = get_most_frequent_words(text_loaded, top_count)
+    print("\n".join(["'{0}': {1}".format(x[0], x[1])
+                     for x in most_frequent_symbols]))
